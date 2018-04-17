29ef995c9f29697ff80f43f5487a7b6eec7e3493
Robert Powell <Powell.Robert@aaa-calif.com>
2018-04-16 20:55:53 -0700

    Completed Chapter-06

--- src/Franc.java
+++ src/Franc.java
@@ -1,6 +1,5 @@
 
-public class Franc {
-	private int amount;
+public class Franc extends Money {
 	
 	Franc(int amount) {
 		this.amount = amount;
@@ -8,10 +7,5 @@ public class Franc {
 
 	Franc times(int multiplier) {
 		return new Franc(amount * multiplier);
-	}
-	
-	public boolean equals(Object object) {
-		Franc franc=(Franc)object;
-		return amount == franc.amount;
 	}
 }
