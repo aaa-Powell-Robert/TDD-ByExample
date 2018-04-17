2d252b41f8671e67364dc05dfe64764ce9572e33
Robert Powell <Powell.Robert@aaa-calif.com>
2018-04-16 21:39:46 -0700

    Completed Chapter-09

--- src/Money.java
+++ src/Money.java
@@ -1,6 +1,16 @@
 
 public abstract class Money {
 	protected int amount;
+	protected String currency;
+	
+	String currency() {
+		return currency;
+	}
+	
+	Money(int amount, String currency) {
+		this.amount = amount;
+		this.currency = currency;
+	}
 
 	abstract Money times(int multiplier);
 	
@@ -11,10 +21,10 @@ public abstract class Money {
 	}
 
 	static Money dollar(int amount) {
-		return new Dollar(amount);
+		return new Dollar(amount, "USD");
 	}
 
 	static Money franc(int amount) {
-		return new Franc(amount);
+		return new Franc(amount, "CHF");
 	}
 }
